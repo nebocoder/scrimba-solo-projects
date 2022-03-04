600885091294106c764bbdf310ca961f539bb81d
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-04 17:46:38 +0100

    Getting movie ID and Info functions

--- watchlist-creator/index.js
+++ watchlist-creator/index.js
@@ -1,1 +1,20 @@
-console.log(config.key);
+const search = document.querySelector('.search--section__input');
+const searchButton = document.querySelector('.search--section__button');
+const main = document.querySelector('.main--section');
+
+async function getMovieIds() {
+  const response = await fetch(
+    `http://www.omdbapi.com/?apikey=${config.key}&s=${search.value}`
+  );
+  const data = await response.json();
+
+  return data.Search.map((movie) => {
+    return movie.imdbID;
+  });
+}
+
+function getMovieInfo(movieId) {
+  fetch(`http://www.omdbapi.com/?apikey=${config.key}&i=${movieId}`)
+    .then((response) => response.json())
+    .then((data) => data);
+}
