15d0fa860522650879e18fea8838201315c14e9f
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-05 16:23:44 +0100

    Card styling and other fixes

--- watchlist-creator/index.js
+++ watchlist-creator/index.js
@@ -13,8 +13,42 @@ async function getMovieIds() {
   });
 }
 
-function getMovieInfo(movieId) {
-  fetch(`http://www.omdbapi.com/?apikey=${config.key}&i=${movieId}`)
-    .then((response) => response.json())
-    .then((data) => data);
+async function getMovieInfo(movieId) {
+  const response = await fetch(
+    `http://www.omdbapi.com/?apikey=${config.key}&i=${movieId}`
+  );
+  const data = await response.json();
+
+  return getMovieHtml(data);
 }
+
+function getMovieHtml(data) {
+  return `
+    <div class="movie--card">
+      <img
+        class="movie--poster"
+        src="${data.Poster}"
+      />
+      <div class="movie--info">
+        <div class="movie--title">
+          <h3>${data.Title}</h3>
+          <img src="images/star.png" />
+          <p>${data.imdbRating}</p>
+        </div>
+        <div class="movie--extra">
+          <p class="movie--length">${data.Runtime}</p>
+          <p class="movie--genre">${data.Genre}</p>
+          <a href="#" class="watchlist">
+            <img class="plus--icon" src="images/plus.png" />
+            <p class="watchlist--action">Watchlist</p>
+          </a>
+        </div>
+        <p class="movie--summary">${data.Plot}</p>
+      </div>
+    </div>
+  `;
+}
+
+async function renderMoviesHtml() {}
+
+searchButton.addEventListener('click', renderMoviesHtml);
